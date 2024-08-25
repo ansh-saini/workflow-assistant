8ec42be289f8353d540055afeb507eab352823f9
Ansh Saini <anshsaini625@gmail.com>
2024-08-25 12:13:22 +0530

    feat: add support for whatsapp messages

--- src/app/api/assistant/route.ts
+++ src/app/api/assistant/route.ts
@@ -1,5 +1,6 @@
 import { openai } from '@ai-sdk/openai';
-import { convertToCoreMessages, streamText } from 'ai';
+import { convertToCoreMessages, generateText, streamText } from 'ai';
+import MessagingResponse from 'twilio/lib/twiml/MessagingResponse';
 import { z } from 'zod';
 
 import { getPersonAvailability } from '@/app/api/assistant/tools/get-person-availability';
@@ -10,40 +11,72 @@ import { scheduleMeeting } from '@/app/api/assistant/tools/schedule-meeting';
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
+  const url = new URL(req.url);
+  const channel = url.searchParams.get('channel');
+
+  const isWhatsapp = channel === 'whatsapp';
+
+  if (isWhatsapp) {
+    const body = await req.text();
+    const message = new URLSearchParams(body).get('Body');
+
+    if (!message) return new Response('message is required', { status: 404 });
+
+    const result = await generateText({
+      model: openai('gpt-4-turbo'),
+      messages: convertToCoreMessages([
+        {
+          role: 'user',
+          content: message,
+        },
+      ]),
+      tools,
+    });
+
+    const twiml = new MessagingResponse();
+    twiml.message(result.text);
+
+    return new Response(twiml.toString(), {
+      headers: { 'Content-Type': 'text/xml' },
+    });
+  }
+
   const { messages } = await req.json();
 
   const result = await streamText({
     model: openai('gpt-4-turbo'),
     messages: convertToCoreMessages(messages),
-    tools: {
-      getPersonName: {
-        description: "Parse the string to extract the user's name.",
-        parameters: z.object({
-          message: z
-            .string()
-            .describe('The message from which to extract the name.'),
-        }),
-        execute: getPersonName,
-      },
-      getPersonAvailability: {
-        description: "Find the user's availability for a meeting.",
-        parameters: z.object({
-          name: z.string().describe('user name'),
-        }),
-        execute: getPersonAvailability,
-      },
-      scheduleMeeting: {
-        description: 'Schedule a meeting with the user.',
-        parameters: z.object({
-          host: z.string().describe('meeting host name'),
-          invitee: z.string().describe('meeting invitee name'),
-          startTime: z.string().describe('meeting start time in format: '),
-          endTime: z.string().describe('meeting end time in format: '),
-        }),
-        execute: scheduleMeeting,
-      },
-    },
+    tools,
   });
 
   return result.toDataStreamResponse();
 }
+
+const tools = {
+  getPersonName: {
+    description: "Parse the string to extract the user's name.",
+    parameters: z.object({
+      message: z
+        .string()
+        .describe('The message from which to extract the name.'),
+    }),
+    execute: getPersonName,
+  },
+  getPersonAvailability: {
+    description: "Find the user's availability for a meeting.",
+    parameters: z.object({
+      name: z.string().describe('user name'),
+    }),
+    execute: getPersonAvailability,
+  },
+  scheduleMeeting: {
+    description: 'Schedule a meeting with the user.',
+    parameters: z.object({
+      host: z.string().describe('meeting host name'),
+      invitee: z.string().describe('meeting invitee name'),
+      startTime: z.string().describe('meeting start time in format: '),
+      endTime: z.string().describe('meeting end time in format: '),
+    }),
+    execute: scheduleMeeting,
+  },
+};
